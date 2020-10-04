d074bae5ea3c70c62a6fb0a5a30b025d2cc5ceef
nikitades <nikitades@pm.me>
2020-10-04 13:30:42 +0300

    Code review: Make the function calls only if needed

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
-function getRandomPercent(){
-    $random_percent =  "Инфа " . rand(1, 100) . " %";
+function getRandomPercent()
+{
+    $random_percent =  rand(1, 100);
     return $random_percent;
 }
 
-$result = getRandomPercent();
-
-$isRandomPercentRequire = isset($_GET['randomPercentRequire']);
+$isRandomPercentRequired = isset($_GET['randomPercentRequire']);
 
 require "main_page.php";
-
--- main_page.php
+++ main_page.php
@@ -3,17 +3,13 @@
 <head>
     <meta charset="utf-8">
     <title>Сколько процентов инфа?</title>
-    <link rel="stylesheet" href="default_style.css">
 </head>
 <body class="background">
     <div class="button_holder">
     <form action="/" method="get">
-    <input type="submit" name="randomPercentRequire" value="1">
+        <input type="submit" name="randomPercentRequire" value="1">
     </form>
     </div>
-    <?php if ($isRandomPercentRequire === TRUE) {
-        echo $result;
-    }
-    ?>
+    <?php echo $isRandomPercentRequired ? ('Инфа ' . getRandomPercent() . '%!') : '' ?>
 </body>
 </html>
